280ff550384c0da5759686b3f17e9d4a720822df
stevengalvis <steven.galvis@gmail.com>
2017-08-26 14:38:18 -0400

    added css for border on cards and wrapper for price and fav icon

--- public/app.js
+++ public/app.js
@@ -1,35 +1,7 @@
 
-
-var colorThief = new ColorThief();
-console.log(colorThief);
 renderFeedItems('pumas');
-//get data from shopstyle api
-// $.ajax({
-//   url: "http://api.shopstyle.com/api/v2/products?pid=uid1025-39588145-82&fts=puma+men + shoe&offset=0&limit=10",
-//   type: "GET",
-//   success: function(data, status) {
-//       console.log(data);
-//       let resultElement = '';
-//       if (data) {
-//         data.products.forEach(function(item) {
-//           let alternateImages = '';
-//           if(item.alternateImages.size !== 0) {
-//             alternateImages = renderAlternateImages(item.alternateImages);
-//           }
-//           resultElement += '<div class="row"><div class ="col-12"><div class="sneaker-card">' +
-//           '<p class ="branded-name">' + item.brandedName + '</p>' +
-//            '<div class = "card-main-image">' + '<a href= "' + item.clickUrl + '">' + '<img src = "' + item.image.sizes.Best.url + '"</img></a></div>' + '<div class ="card-content">' + '<div class = "js-alternate-images">' + alternateImages + '</div>' +
-//           '<p class ="price-label">' + item.priceLabel + '</p></div>' +
-//           '<button type ="button" class ="favorite-button" title="Add to favorites">' +
-//           '<i class="fa fa-heart-o" aria-hidden="true"></i></button></div></div></div>';
-//         });
-//       }
-//       else {
-//         resultElement += '<p>No results</p>';
-//       }
-//       $('.js-sneaker-results').html(resultElement);
-//   }
-// });
+
+
 
 //get search query
 $('.search').on('submit', function(e) {
@@ -62,21 +34,18 @@ function renderFeedItems(searchQuery) {
         if (data) {
           data.products.forEach(function(item) {
             let alternateImages = '';
-            // let productImg = item.image.sizes.Best.url;
-            // console.log(productImg);
-            // let color = colorThief.getColor(productImg);
-            // console.log(color);
+
             if(item.alternateImages.size !== 0) {
               alternateImages = renderAlternateImages(item.alternateImages);
             }
             resultElement += '<div class="row"><div class ="col-12"><div class="sneaker-card">' +
             '<p class ="branded-name">' + item.brandedName + '</p>' +
-             '<div class = "card-main-image">' + '<a href= "' + item.clickUrl + '">' + '<img src = "' + item.image.sizes.Best.url + '"</img></a></div>' + '<div class ="card-content">' + '<div class = "js-alternate-images">' + alternateImages + '</div>' +
+             '<div class ="card-main-image">' +
+             '<a href= "' + item.clickUrl + '">' + '<img src = "' + item.image.sizes.Best.url + '"</img></a></div>' + '<div class ="card-content">' + '<div class = "js-alternate-images">' + alternateImages + '</div>' +
             '<p class ="price-label">' + item.priceLabel + '</p></div>' +
+            '<div class ="add-to-favorites">' +
             '<button type ="button" class ="favorite-button" title="Add to favorites">' +
-            '<i class="fa fa-heart-o" aria-hidden="true"></i></button></div></div></div>';
-            let img = $('.card-main-image').find('a').children('img').attr('src');
-            console.log($('.card-main-image').closest('a'));
+            '<i class="fa fa-heart-o" aria-hidden="true"></i></button></div></div></div></div>';
           });
         }
         else {
@@ -86,6 +55,7 @@ function renderFeedItems(searchQuery) {
     }
   });
 }
+
 
 
 //dom manipulation
@@ -201,7 +171,6 @@ $('.tab a').on('click', function (e) {
     // event listener for favorites
     //get data from resultElement
     $('.sneaker-card').on('click','.favorite-button', function(e) {
-      console.log($(this).siblings('.card-main-image').find('a').attr('href'));
 
       let shoeData = {
         brandedName: $(this).closest('.sneaker-card').find('.branded-name').text(),
