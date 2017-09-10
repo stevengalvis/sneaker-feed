c3a417ef768c27ced67b67075815168b0b610b38
stevengalvis <steven.galvis@gmail.com>
2017-09-10 14:16:55 -0400

    fixed bugs on favorites page

--- public/browse.js
+++ public/browse.js
@@ -125,63 +125,63 @@ $('.form').find('input, textarea').on('keyup blur focus', function(e) {
       }
 });
 
-//sign up and login form switching
-$('.tab a').on('click', function (e) {
-
-  e.preventDefault();
-
-  $(this).parent().addClass('active');
-  $(this).parent().siblings().removeClass('active');
-
-  let target = $(this).attr('href');
-  $('.tab-content > div').not(target).hide();
-
-  $(target).fadeIn(600);
-
-});
-
-    $(".sign-up-user").on('submit', function(e) {
-      e.preventDefault();
-        console.log('clicked');
-        let data = {
-            username: $("input[name=username]").val(),
-            firstName: $("input[name=firstName]").val(),
-            lastName: $("input[name=lastName]").val(),
-            password: $("input[name=password]").val()
-        }
-
-        $.ajax({
-            url: "http://localhost:8080/users",
-            type: "POST",
-            data: JSON.stringify(data),
-            contentType: "application/json",
-            success: function(data, status) {
-                console.log(data, status);
-            }
-        });
-
-    });
-
-    //event listener for logging in
-
-    $('.login-user').on('submit', function(e) {
-      e.preventDefault();
-      const username = $("input[name=username]").val();
-      const password = $("input[name=password]").val();
-
-      $.ajax({
-        type: "GET",
-        url: 'http://localhost:8080/users/login',
-        headers: {
-          'content-type': "application/json",
-          authorization: "Basic " + btoa(username + ':' + password)
-        },
-        contentType: 'application/json',
-        success: function(data, status) {
-          console.log(data, status);
-        }
-      });
-    });
+// //sign up and login form switching
+// $('.tab a').on('click', function (e) {
+//
+//   e.preventDefault();
+//
+//   $(this).parent().addClass('active');
+//   $(this).parent().siblings().removeClass('active');
+//
+//   let target = $(this).attr('href');
+//   $('.tab-content > div').not(target).hide();
+//
+//   $(target).fadeIn(600);
+//
+// });
+//
+//     $(".sign-up-user").on('submit', function(e) {
+//       e.preventDefault();
+//         console.log('clicked');
+//         let data = {
+//             username: $("input[name=username]").val(),
+//             firstName: $("input[name=firstName]").val(),
+//             lastName: $("input[name=lastName]").val(),
+//             password: $("input[name=password]").val()
+//         }
+//
+//         $.ajax({
+//             url: "http://localhost:8080/users",
+//             type: "POST",
+//             data: JSON.stringify(data),
+//             contentType: "application/json",
+//             success: function(data, status) {
+//                 console.log(data, status);
+//             }
+//         });
+//
+//     });
+//
+//     //event listener for logging in
+//
+//     $('.login-user').on('submit', function(e) {
+//       e.preventDefault();
+//       const username = $("input[name=username]").val();
+//       const password = $("input[name=password]").val();
+//
+//       $.ajax({
+//         type: "GET",
+//         url: 'http://localhost:8080/users/login',
+//         headers: {
+//           'content-type': "application/json",
+//           authorization: "Basic " + btoa(username + ':' + password)
+//         },
+//         contentType: 'application/json',
+//         success: function(data, status) {
+//           console.log(data, status);
+//         }
+//       });
+//     });
 
 
     // event listener for logging out
@@ -203,7 +203,7 @@ $('.tab a').on('click', function (e) {
 
     // event listener for favorites
     //get data from resultElement
-    $('.sneaker-card').on('click','.favorite-button', function(e) {
+    $('.js-sneaker-results').on('click','.favorite-button', function(e) {
       console.log('clicked');
       let shoeData = {
         brandedName: $(this).closest('.sneaker-card').find('.branded-name').text(),
