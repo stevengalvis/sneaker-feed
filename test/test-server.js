4658a10d148689a357b6724dbd678ba4850e7cbd
stevengalvis <steven.galvis@gmail.com>
2018-01-27 18:19:00 -0500

    made changest on test file

--- test/test-server.js
+++ test/test-server.js
@@ -1,27 +1,28 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 
 const should = chai.should();
 
-const {app, runServer, closeServer} = require('../server');
+const { app, runServer, closeServer } = require("../server");
+const { DATABASE_URL } = require("../config");
 
 chai.use(chaiHttp);
 
-describe('Testing server', function() {
+describe("Testing server", function() {
   before(function() {
-    return runServer();
+    return runServer(DATABASE_URL);
   });
 
   after(function() {
     return closeServer();
   });
 
-  it('should return 200 status code on GET to root url', function() {
-    return chai.request(app)
-    .get('/')
-    .then(function(res) {
-      res.should.have.status(200);
-    });
+  it("should return 200 status code on GET to root url", function() {
+    return chai
+      .request(app)
+      .get("/")
+      .then(function(res) {
+        res.should.have.status(200);
+      });
   });
-
 });
